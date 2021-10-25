0581b39940685a34947f81f6a668cac66e70ee1b
Ivan <ivanengels1995@gmail.com>
2021-10-25 21:28:00 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,9 +12,36 @@ public class Main {
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
 
+        for (int i = 100; i >= 0; i--) {
+            System.out.print("\n" + i);
+            if (i % 8 == 0) {
+                System.out.print(" - podzilone przez 8");
+            }
+        }
+
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+
+        int[] a = new int[5];
+        int i = 0;
+        int m = 11;
+
+        Scanner in = new Scanner(System.in);
+
+        for (int element : a) {
+
+            System.out.println("\nPodaj " + (i + 1) + " element  tablicy: ");
+            a[i] = in.nextInt();
+            i++;
+        }
+        System.out.println(Arrays.toString(a));
+
+
+        for (int element: a) {
+            System.out.println(element + 11);
+        }
+
 
     }
 }
