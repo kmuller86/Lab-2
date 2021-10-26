afb207974a920f48e67216661d9040405656f78f
Ivan <ivanengels1995@gmail.com>
2021-10-26 19:42:17 +0200

    3

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,64 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbe:");
+        int a = scan.nextInt();
+        int b;
+        switch (a % 2) {
+            case 0:
+                System.out.print("Parzysta");
+                break;
+            default:
+                System.out.print("Nieparzysta");
+                break;
+        }
+        switch (a%7){
+            case 0:
+                System.out.print("\nLiczba  podzielona przez 7");
+                break;
+            default:
+                System.out.print("\nLiczba nie podzielona przez 7");
+
+        }
+
 
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
+
+          String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+
+          int tablica [] = {0,0,0,0,0,0};
+          for (char litera: text.toCharArray ()){
+          switch (litera){
+              case 'o':
+                  ++tablica[0];
+                  break;
+              case'p':
+                  ++tablica[1];
+                  break;
+              case'x':
+                  ++tablica[2];
+                  break;
+              case'z':
+                  ++tablica[3];
+                  break;
+              case'q':
+                  ++tablica[4];
+                  break;
+              default :
+                  ++tablica[5];
+
+
+          }
+
+          }
+        System.out.printf("\nLiczba występuje:\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\nq: %d - razy\npozostale litery: %d - liter",
+                tablica[0], tablica[1], tablica[2],tablica[3],tablica[4],tablica[5]);
+
 
     }
 }
