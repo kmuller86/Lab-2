1646904f24641fe18881c1f984063267535eadc5
Ivan <ivanengels1995@gmail.com>
2021-10-27 18:57:29 +0200

    3

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,27 @@ public class Main {
         /* zad.1 napisać 3 pętle for i użyc:
         a) w pierwszej pętli break
         b) w drugiej pętli continue
-        c) w trzeciej pętli return
-        */
+        c) w trzeciej pętli return*/
+        System.out.print("break:\n");
+        for( int a=0;a<=9;a++){
+            if(a==7)
+                break;
+                System.out.println(a + " ");
+
+        }
+        System.out.print("continue:\n");
+        for (int b = 0;b<=99;b++){
+            if(b%94==0)
+                continue;
+            System.out.println(b + " ");
+
+        }
+        System.out.print("return:\n");
+        for (int c = 55;c>=0;c--){
+            if(c==42)
+                return;
+            System.out.println(c + " ");
+        }
 
     }
 }
