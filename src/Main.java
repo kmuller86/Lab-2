8537f9b595afa8e75a34102706d95d1e2708a128
Ivan <ivanengels1995@gmail.com>
2021-10-31 15:23:13 +0100

    6

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,43 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.security.PublicKey;
 import java.util.*;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-
         /* ćw_6 Listy */
+       System.out.println("Wpisz imiena zwierzat: ");
+       Scanner in = new Scanner(System.in);
+       List<String> zwierzata = new ArrayList<>();
 
-        /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
-                Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
-                elementów i elementami mają być nazwy zwierząt.
-                b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
-                -- podpowiedź: for(String zmienna : List<String>)
-                c) proszę usunąć 2 ostatnie elementy listy i dodać do niej 3 inne elementy,
-                następnie wyświetlić listę i wielkość listy,
-                d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
-                elementów listy
-          */
+       for (int i=0;i<5;i++){
+           String name = in.nextLine();
+           zwierzata.add(name);
 
-
-         /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
-                niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
-                 a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
-         */
-
+       }
+        methoda(zwierzata);
+        zwierzata.remove(4);
+        zwierzata.remove(3);
+        zwierzata.add("Bonia");
+        zwierzata.add("Monia");
+        zwierzata.add("Dagir");
+        System.out.println(zwierzata+",wielkość listy - "+zwierzata.size());
+        methoda1(zwierzata);
+    }
+        public static void methoda(List<String> zwierzata){
+        for(String name :zwierzata){
+            System.out.println("Ma na Imie:\n"+name);
+        }
 
     }
+    public static void methoda1 (List<String>zwierzata){
+       Collections.reverse(zwierzata);
+       System.out.println(zwierzata);
+    }
+
+
 
 }
