82d535863f67f58f9aa92e81dc6b950c84dec638
Ivan <ivanengels1995@gmail.com>
2021-10-31 09:11:22 +0100

    5

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,7 @@ public class Main {
     public static void main(String[] args) {
         /* ćwiczenie 5 Metody */
 
-        /* zad.1
-        a) stworzyć tablicę typu int[20],
-        b) napisać metodę która będzie uzupełniała tablicę wartościami od 40 do 20,
-        c) napisać metodę, która wypisze otrzymane wartości,
-        d) wstawić wywołanie metody do metody main
-        */
+
         methoda2(methoda1());
         String a []= new String[6];
         methoda3(a);
@@ -37,12 +32,7 @@ public class Main {
         }
      }
 
-    /* zad.2
-    a) stworzyc tablicę typu String[6],
-    b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury
-        (użyć do tego pętli i klasy Scanner),
-    c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
-    */
+
      public static void  methoda3  (String[] a){
         Scanner in = new Scanner(System.in);
         int i=0;
@@ -59,14 +49,7 @@ public class Main {
 
         }
     }
-    /* zad.3
-        a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
-            wyświetla w konsoli string który ma być waszym imieniem,
-        b) napisać metodę o tej samej nazwie jak w punkcie a) z 2-oma parametrami typu String, która wyświetli
-            wasze imie i nazwisko,
-        c) napisać metodę o tej samej nazwie jak w punkcie a) i b) która przyjmuje wasze imie,
-            nazwisko i wiek (int), a następnie wszystko wyświetli,
-        */
+
     public static void methoda5(String imie){
          System.out.println("Mam na imie "+imie);
 
@@ -79,19 +62,7 @@ public class Main {
 
          System.out.println("Mam na Imie "+imie+" moje Nazwisko "+nazwisko+" mam "+wiek);
     }
-    /* zad.4
-       a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
-           coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
-       b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
-           którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
-       c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
-       */ /* zad.4
-        a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
-            coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
-        b) napisać metodę (nazwa ta sama co w punkcie a) która przyjmuje 3 parametry typu int i zwraca ich sumę,
-            którą przypiszemy do zmiennej, coś takiego:     zmiennaB = metoda(7, 8, 9); <-- wywołanie metody
-        c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
-        */
+
     public static int methoda6(int a , int b){
       return a+b;
      }
