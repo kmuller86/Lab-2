404e5915584a5d0f531e0dbdf67b25f32cbf81a5
Ivan <ivanengels1995@gmail.com>
2021-10-31 16:29:08 +0100

    6 Zadania (2)

--- src/Main.java
+++ src/Main.java
@@ -8,35 +8,26 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("Podaj 10 liczb");
+        Scanner in = new Scanner (System.in);
+        Set<Integer> number = new TreeSet<>();
 
-       System.out.println("Wpisz imiena zwierzat: ");
-       Scanner in = new Scanner(System.in);
-       List<String> zwierzata = new ArrayList<>();
-
-       for (int i=0;i<5;i++){
-           String name = in.nextLine();
-           zwierzata.add(name);
-
-       }
-        methoda(zwierzata);
-        zwierzata.remove(4);
-        zwierzata.remove(3);
-        zwierzata.add("Bonia");
-        zwierzata.add("Monia");
-        zwierzata.add("Dagir");
-        System.out.println(zwierzata+",wielkość listy - "+zwierzata.size());
-        methoda1(zwierzata);
-    }
-        public static void methoda(List<String> zwierzata){
-        for(String name :zwierzata){
-            System.out.println("Ma na Imie:\n"+name);
+        for(int i = 0;i<10;i++){
+            int numer = in.nextInt();
+            number.add(numer);
         }
 
-    }
-    public static void methoda1 (List<String>zwierzata){
-       Collections.reverse(zwierzata);
-       System.out.println(zwierzata);
-    }
+        System.out.println("Liczby: "+number);
+        methoda(number);
+     }
+     public static void methoda(Set<Integer> number){
+        int i=0;
+        for(Integer x :number){
+            System.out.println("Liczba "+(i+1)+" :"+x);
+            i++;
+        }
+     }
+
 
 
 
